b2abcf648c5ed316438e60682a4be0afeda934fc
sergiumr <s3rgiumr@gmail.com>
2021-11-23 22:32:10 +0200

    code improvements

--- README.md
+++ README.md
@@ -1,14 +1,26 @@
 # minefield
+
 Minfield in Python3
 
 # GUI
+
 taken from: https://github.com/KenoLeon/Medium-Mazes
 
 # steps to run
+
 1. clone/ download the project
 2. open folder with vscode/ pycharm
 3. python -m venv venv
 4. .\venv\Scripts\activate
 5. pip install PySimpleGUI
 6. pip install numpy
-7. python minefield.py
+7. pip install pillow
+8. python minefield.py
+
+# problems
+
+1. reading map from file
+2. images are not displayed correctly
+3. what if pickaxe reaches an image?
+4. after map, messages should be:
+   i j message
--- minefield.py
+++ minefield.py
@@ -1,166 +1,194 @@
-import PySimpleGUI as sg
-import numpy as np
+import os
 import math
-import random
+import numpy
+import PySimpleGUI
+from tkinter import *
+from PIL import ImageTk, Image
+
+RESOURCES_FOLDER = './resources'
+MAP_TEST = 'map_test.txt'
+PICKAXE = 'pickaxe.png'
+MESSAGE = 'message.png'
+BOMB = 'bomb.png'
+
+UTILS = {'cell_count': 0,
+         'cell_size': 0,
+         'grid_size': 400,
+         'canvas': False,
+         'window': False,
+         'player_pos': [0, 0],
+         'cell_map': False,
+         'safe_map': False,
+         'visited_map': False,
+         'messages_coords': False,
+         'bombs_coords': False,
+         }
+
+APP_FONT = 'Any 16'
+PySimpleGUI.theme('DarkGrey5')
+SCORE = 0
 
 
-AppFont = 'Any 16'
-sg.theme('DarkGrey5')
-_VARS = {'cellCount': 10, 'gridSize': 400, 'canvas': False, 'window': False,
-         'playerPos': [0, 0], 'cellMAP': False}
-cellSize = _VARS['gridSize']/_VARS['cellCount']
-exitPos = [_VARS['cellCount']-1, _VARS['cellCount']-1]
+def draw_grid():
+    UTILS['canvas'].TKCanvas.create_rectangle(
+        1, 1, UTILS['grid_size'], UTILS['grid_size'], outline='BLACK', width=1)
 
-
-
-def makeMaze(dimX, dimY):
-    """makes a maze by adding walls and holes in walls.
-
-    Adds random vertical and horizontal lines and then cuts holes randomly.
-    Mostly works, but not perfect.
-
-    """
-
-    # Make a starter gir of zeros:
-    starterMap = np.zeros((dimX, dimY), dtype=int)
-    # add rows and columns:
-    for x in range(2):
-        randRow = random.randint(1, dimX)
-        randColumn = random.randint(1, dimY)
-        starterMap[randRow-1:randRow] = 1
-        starterMap[:, randColumn-1] = 1
-        # poke holes in said rows and columns:
-        for x in range(4):
-            starterMap[randRow-1][random.randint(0, dimY-1)] = 0
-            starterMap[random.randint(0, dimX-1)][randColumn-1] = 0
-    # Add blank cells fro entrance,exit and around them:
-    starterMap[0][0] = 0
-    starterMap[0][1] = 0
-    starterMap[1][0] = 0
-    starterMap[dimX-1][dimY-1] = 0
-    starterMap[dimX-1][dimY-2] = 0
-    starterMap[dimX-2][dimY-1] = 0
-    starterMap[dimX-2][dimY-2] = 0
-    # print (starterMap)
-    return starterMap
-
-
-_VARS['cellMAP'] = makeMaze(_VARS['cellCount'], _VARS['cellCount'])
-
-
-# METHODS:
-
-
-def drawGrid():
-    cells = _VARS['cellCount']
-    _VARS['canvas'].TKCanvas.create_rectangle(
-        1, 1, _VARS['gridSize'], _VARS['gridSize'], outline='BLACK', width=1)
-    for x in range(cells):
-        _VARS['canvas'].TKCanvas.create_line(
-            ((cellSize * x), 0), ((cellSize * x), _VARS['gridSize']),
+    for idx in range(UTILS['cell_count']):
+        UTILS['canvas'].TKCanvas.create_line(
+            ((UTILS['cell_size'] * idx),
+             0), ((UTILS['cell_size'] * idx), UTILS['grid_size']),
             fill='BLACK', width=1)
-        _VARS['canvas'].TKCanvas.create_line(
-            (0, (cellSize * x)), (_VARS['gridSize'], (cellSize * x)),
+        UTILS['canvas'].TKCanvas.create_line(
+            (0, (UTILS['cell_size'] * idx)
+             ), (UTILS['grid_size'], (UTILS['cell_size'] * idx)),
             fill='BLACK', width=1)
 
 
-def drawCell(x, y, color='GREY'):
-    _VARS['canvas'].TKCanvas.create_rectangle(
-        x, y, x + cellSize, y + cellSize,
+def draw_image(x, y, resource):
+    x *= UTILS['cell_size']
+    y *= UTILS['cell_size']
+
+    image = Image.open(os.path.join(RESOURCES_FOLDER, resource))
+    image = image.resize(
+        (UTILS['cell_size'], UTILS['cell_size']), Image.ANTIALIAS)
+    photoImage = ImageTk.PhotoImage(image)
+    UTILS['canvas'].TKCanvas.create_image(
+        x, y, image=photoImage, anchor='nw')
+
+
+def draw_cell(x, y, color='GREY'):
+    x *= UTILS['cell_size']
+    y *= UTILS['cell_size']
+
+    UTILS['canvas'].TKCanvas.create_rectangle(
+        x, y, x + UTILS['cell_size'], y + UTILS['cell_size'],
         outline='BLACK', fill=color, width=1)
 
 
-def placeCells():
-    for row in range(_VARS['cellMAP'].shape[0]):
-        for column in range(_VARS['cellMAP'].shape[1]):
-            if(_VARS['cellMAP'][column][row] == 1):
-                drawCell((cellSize*row), (cellSize*column))
+def initialize_map():
+    UTILS['cell_map'] = numpy.zeros(
+        (UTILS['cell_count'], UTILS['cell_count']), dtype=int)
+    UTILS['visited_map'] = numpy.zeros(
+        (UTILS['cell_count'], UTILS['cell_count']), dtype=int)
+
+    layout = [[PySimpleGUI.Canvas(size=(UTILS['grid_size'], UTILS['grid_size']),
+                                  background_color='WHITE',
+                                  key='canvas')],
+              [PySimpleGUI.Exit(font=APP_FONT),
+               PySimpleGUI.Text('', key='-SCORE-',
+                                font=APP_FONT, size=(15, 1)),
+               PySimpleGUI.Button('Restart', font=APP_FONT)]]
+
+    UTILS['window'] = PySimpleGUI.Window('Minefield v1.0', layout, resizable=True, finalize=True,
+                                         return_keyboard_events=True)
+    UTILS['canvas'] = UTILS['window']['canvas']
+
+    image = Image.open(os.path.join(RESOURCES_FOLDER, PICKAXE))
+    image = image.resize(
+        (UTILS['cell_size'], UTILS['cell_size']), Image.ANTIALIAS)
+    photoImage = ImageTk.PhotoImage(image)
+    UTILS['canvas'].TKCanvas.create_image(
+        UTILS['player_pos'][0], UTILS['player_pos'][1], image=photoImage, anchor='nw')
+
+    draw_grid()
+    draw_map()
 
 
-def checkEvents(event):
-    move = ''
-    if len(event) == 1:
-        if ord(event) == 63232:  # UP
-            move = 'Up'
-        elif ord(event) == 63233:  # DOWN
-            move = 'Down'
-        elif ord(event) == 63234:  # LEFT
-            move = 'Left'
-        elif ord(event) == 63235:  # RIGHT
-            move = 'Right'
-    # Filter key press Windows :
-    else:
-        if event.startswith('Up'):
-            move = 'Up'
-        elif event.startswith('Down'):
-            move = 'Down'
-        elif event.startswith('Left'):
-            move = 'Left'
-        elif event.startswith('Right'):
-            move = 'Right'
+def draw_map():
+    for i in range(UTILS['cell_count']):
+        for j in range(UTILS['cell_count']):
+            if UTILS['visited_map'][i][j] == 1:
+                draw_cell(i, j)
+            if (i, j) in UTILS['messages_coords']:
+                draw_cell(i, j, 'GREEN')
+                # draw_image(i, j, MESSAGE)
+            if (i, j) in UTILS['bombs_coords']:
+                draw_cell(i, j, 'RED')
+                # draw_image(i, j, BOMB)
+
+
+def get_event(event):
+    move = event
+    if event.startswith('Up'):
+        move = 'Up'
+    elif event.startswith('Down'):
+        move = 'Down'
+    elif event.startswith('Left'):
+        move = 'Left'
+    elif event.startswith('Right'):
+        move = 'Right'
+
     return move
 
 
-# INIT :
-layout = [[sg.Canvas(size=(_VARS['gridSize'], _VARS['gridSize']),
-                     background_color='white',
-                     key='canvas')],
-          [sg.Exit(font=AppFont),
-           sg.Text('', key='-exit-', font=AppFont, size=(15, 1)),
-           sg.Button('NewMaze', font=AppFont)]]
-
-_VARS['window'] = sg.Window('Random Puzzle Generator', layout, resizable=True, finalize=True,
-                            return_keyboard_events=True)
-_VARS['canvas'] = _VARS['window']['canvas']
-drawGrid()
-drawCell(_VARS['playerPos'][0], _VARS['playerPos'][1], 'TOMATO')
-drawCell(exitPos[0]*cellSize, exitPos[1]*cellSize, 'Black')
-placeCells()
-
-
-while True:             # Event Loop
-    event, values = _VARS['window'].read()
+def process_events():
+    event, _ = UTILS['window'].read()
     if event in (None, 'Exit'):
-        break
+        return -1
 
-    if event == 'NewMaze':
-        _VARS['playerPos'] = [0, 0]
-        _VARS['cellMAP'] = makeMaze(_VARS['cellCount'], _VARS['cellCount'])
-    
-    # Filter key press
-    xPos = int(math.ceil(_VARS['playerPos'][0]/cellSize))
-    yPos = int(math.ceil(_VARS['playerPos'][1]/cellSize))    
+    event_type = get_event(event)
+    if event_type == 'Up' and int(UTILS['player_pos'][1] - UTILS['cell_size']) >= 0:
+        UTILS['player_pos'][1] = UTILS['player_pos'][1] - UTILS['cell_size']
+    elif get_event(event) == 'Down' and int(UTILS['player_pos'][1] + UTILS['cell_size']) < UTILS['grid_size']-1:
+        UTILS['player_pos'][1] = UTILS['player_pos'][1] + UTILS['cell_size']
+    elif event_type == 'Left' and int(UTILS['player_pos'][0] - UTILS['cell_size']) >= 0:
+        UTILS['player_pos'][0] = UTILS['player_pos'][0] - UTILS['cell_size']
+    elif event_type == 'Right' and int(UTILS['player_pos'][0] + UTILS['cell_size']) < UTILS['grid_size']-1:
+        UTILS['player_pos'][0] = UTILS['player_pos'][0] + UTILS['cell_size']
+    elif event_type == 'Restart':
+        UTILS['cell_map'] = numpy.zeros(
+            (UTILS['cell_count'], UTILS['cell_count']), dtype=int)
+        UTILS['visited_map'] = numpy.zeros(
+            (UTILS['cell_count'], UTILS['cell_count']), dtype=int)
+        UTILS['messages_coords'] = []
+        UTILS['bombs_coords'] = []
 
-    if checkEvents(event) == 'Up':
-        if int(_VARS['playerPos'][1] - cellSize) >= 0:
-            if _VARS['cellMAP'][yPos-1][xPos] != 1:
-                _VARS['playerPos'][1] = _VARS['playerPos'][1] - cellSize
-    elif checkEvents(event) == 'Down':
-        if int(_VARS['playerPos'][1] + cellSize) < _VARS['gridSize']-1:
-            if _VARS['cellMAP'][yPos+1][xPos] != 1:
-                _VARS['playerPos'][1] = _VARS['playerPos'][1] + cellSize
-    elif checkEvents(event) == 'Left':
-        if int(_VARS['playerPos'][0] - cellSize) >= 0:
-            if _VARS['cellMAP'][yPos][xPos-1] != 1:
-                _VARS['playerPos'][0] = _VARS['playerPos'][0] - cellSize
-    elif checkEvents(event) == 'Right':
-        if int(_VARS['playerPos'][0] + cellSize) < _VARS['gridSize']-1:
-            if _VARS['cellMAP'][yPos][xPos+1] != 1:
-                _VARS['playerPos'][0] = _VARS['playerPos'][0] + cellSize
 
-    # Clear canvas, draw grid and cells
-    _VARS['canvas'].TKCanvas.delete("all")
-    drawGrid()
-    drawCell(exitPos[0]*cellSize, exitPos[1]*cellSize, 'Black')
-    drawCell(_VARS['playerPos'][0], _VARS['playerPos'][1], 'TOMATO')
-    placeCells()
+def run():
+    while True:
+        UTILS['canvas'].TKCanvas.delete("all")
 
-    # Check for Exit:
-    xPos = int(math.ceil(_VARS['playerPos'][0]/cellSize))
-    yPos = int(math.ceil(_VARS['playerPos'][1]/cellSize))
-    if [xPos, yPos] == exitPos:
-        _VARS['window']['-exit-'].update('Found the exit !')
-    else:
-        _VARS['window']['-exit-'].update('')
+        UTILS['window']['-SCORE-'].update(f'Score: {SCORE}')
 
-_VARS['window'].close()
+        draw_grid()
+
+        xPos = UTILS['player_pos'][0] // UTILS['cell_size']
+        yPos = UTILS['player_pos'][1] // UTILS['cell_size']
+
+        UTILS['visited_map'][xPos][yPos] = 1
+
+        draw_map()
+
+        draw_image(UTILS['player_pos'][0], UTILS['player_pos'][1], PICKAXE)
+
+        if process_events() == -1:
+            break
+
+    UTILS['window'].close()
+
+
+def read_config(map_path):
+    UTILS['messages_coords'] = []
+    UTILS['bombs_coords'] = []
+
+    row = 0
+    with open(map_path, "r") as map_file:
+        for line in map_file.readlines():
+            for column in range(len(line)):
+                if line[column] == 'M':
+                    UTILS['messages_coords'].append((row, column))
+                elif line[column] == 'B':
+                    UTILS['bombs_coords'].append((row, column))
+
+            row += 1
+
+    UTILS['cell_count'] = row
+    UTILS['cell_size'] = UTILS['grid_size'] // UTILS['cell_count']
+
+
+if __name__ == "__main__":
+    read_config(MAP_TEST)
+
+    initialize_map()
+
+    run()
